2a338308382575eacf272f0473b310ddf8f7a485
bardhideveloper <97695186+bardhideveloper@users.noreply.github.com>
2022-11-17 16:14:29 +0100

    Delete webMain.java

--- webMain.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-public class webMain {
-    public static void main (String args []) {
-        webBrowser browser = new webBrowser();
-        browser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        browser.setSize(500, 500);
-        browser.setVisible(true);
-    }
-}
