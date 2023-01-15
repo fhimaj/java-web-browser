276df03c24ab5fca1f6d25b50004ac57ecc0ad48
fhimaj <95918518+fhimaj@users.noreply.github.com>
2023-01-15 20:42:24 +0100

    Updates.

--- JavaWebBrowser/src/main/java/org/example/Bookmark.java
+++ JavaWebBrowser/src/main/java/org/example/Bookmark.java
@@ -1,3 +1,4 @@
+package org.example;
 public class Bookmark {
 
     //name of bookmark
@@ -18,5 +19,24 @@ public class Bookmark {
 
     public String getUrl() {
         return url;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==null){
+            return false;
+        }
+        if (!(obj instanceof Bookmark)) {
+            return false;
+        }
+
+        Bookmark other = (Bookmark) obj;
+
+       if(this.name!=null && this.name.equals(other.name) && this.url!=null && this.url.equals(other.url)){
+           return true;
+       }
+       else{
+           return false;
+       }
+
     }
 }
